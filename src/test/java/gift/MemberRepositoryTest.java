2b2ce328443f4c23c666118d0953dc318df5e2a7
hopencode <newalfo1006@gmail.com>
2025-07-17 15:04:29 +0900

    feat: MemberRepository @DataJpaTest 코드 작성

---
+++ src/test/java/gift/MemberRepositoryTest.java
@@ -0,0 +1,52 @@
+package gift;
+
+import gift.entity.Member;
+import gift.repository.MemberRepository;
+import org.h2.engine.Role;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class MemberRepositoryJpaTest {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void 회원_계정_정상_저장() {
+        Member member = new Member(null, "test@email.com", "12345678");
+
+        var actual = memberRepository.save(member);
+
+        assertAll(
+                () -> assertThat(actual.getId()).isNotNull(),
+                () -> assertThat(actual.getEmail()).isEqualTo(member.getEmail())
+        );
+    }
+
+    @Test
+    void 회원_계정_정상_검색() {
+        Member member = new Member(null, "test@email.com", "12345678");
+        memberRepository.save(member);
+
+        Optional<Member> found = memberRepository.findByEmail("test@email.com");
+
+        assertThat(found).isPresent();
+        assertThat(found.get().getEmail()).isEqualTo("test@email.com");
+    }
+
+    @Test
+    public void 없는_회원_계정_검색() {
+        Optional<Member> found = memberRepository.findByEmail("not_exist@email.com");
+
+        assertThat(found).isNotPresent();
+    }
+}
