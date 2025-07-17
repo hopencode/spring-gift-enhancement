a57e001071d97c91bd9a9ede8a24cc8ba5962864
hopencode <newalfo1006@gmail.com>
2025-07-17 15:19:17 +0900

    feat: ProductRepositoryTest에 전체 조회, 수정, 삭제 테스트 추가

--- src/test/java/gift/ProductRepositoryTest.java
+++ src/test/java/gift/ProductRepositoryTest.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
@@ -17,7 +20,7 @@ class ProductRepositoryTest {
 
     @Test
     void 상품_저장_정상_테스트() {
-        Product product = new Product(null, "저장테스트", 1000, "https://테스트.png");
+        Product product = new Product(null, "저장테스트", 1000, "https://저장테스트.png");
 
         var result = productRepository.save(product);
 
@@ -28,8 +31,8 @@ class ProductRepositoryTest {
     }
 
     @Test
-    void 저장된_상품_정상_id_검색_테스트() {
-        Product product = new Product(null, "검색테스트", 1000, "https://테스트.png");
+    void 저장된_상품_id_검색_정상_테스트() {
+        Product product = new Product(null, "검색테스트", 1000, "https://검색테스트.png");
 
         var saved = productRepository.save(product);
 
@@ -37,5 +40,47 @@ class ProductRepositoryTest {
 
         assertThat(found).isNotNull();
         assertThat(found).isEqualTo(product.getName());
+    }
+
+    @Test
+    public void 저장된_상품_전체_목록_조회_정상_테스트() {
+
+        Product product1 = new Product(null, "조회테스트1", 1000, "https://조회테스트1.png");
+        Product product2 = new Product(null, "조회테스트2", 1500, "https://조회테스트2.png");
+
+        productRepository.save(product1);
+        productRepository.save(product2);
+
+        List<Product> products = productRepository.findAll();
+
+        assertThat(products).hasSize(2);
+        assertThat(products).extracting("name").contains("조회테스트1", "조회테스트2");
+    }
+
+    @Test
+    public void 상품_수정_정상_테스트() {
+        Product product = new Product(null, "수정테스트1", 1000, "https://수정테스트2.png");
+        var saved = productRepository.save(product);
+
+        Product updateProduct = new Product(saved.getId(), "수정테스트2", 1500, "https://수정테스트2.png");
+        productRepository.save(updateProduct);
+
+        Optional<Product> updated = productRepository.findById(product.getId());
+
+        assertThat(updated).isPresent();
+        assertThat(updated.get().getName()).isEqualTo("수정테스트2");
+        assertThat(updated.get().getPrice()).isEqualTo(1500);
+    }
+
+    @Test
+    public void 상품_삭제_정상_테스트() {
+        Product product = new Product(null, "삭제테스트", 1000, "https://삭제테스트.png");
+        var saved = productRepository.save(product);
+        Long id = saved.getId();
+
+        productRepository.deleteById(id);
+
+        Optional<Product> deleted = productRepository.findById(id);
+        assertThat(deleted).isNotPresent();
     }
 }
