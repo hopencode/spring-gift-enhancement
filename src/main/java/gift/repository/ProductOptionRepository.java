7b54829d8339ccb58d8c96575b267980b38e42c1
hopencode <newalfo1006@gmail.com>
2025-07-22 16:52:02 +0900

    refactor: EntityGraph 적용

--- src/main/java/gift/repository/ProductOptionRepository.java
+++ src/main/java/gift/repository/ProductOptionRepository.java
@@ -9,9 +9,9 @@ import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
-    //@EntityGraph(attributePaths = "product")
+    @EntityGraph(attributePaths = "product")
     Page<ProductOption> findAllByProductId(Long productId, Pageable pageable);
 
-    //@EntityGraph(attributePaths = "product")
+    @EntityGraph(attributePaths = "product")
     Optional<ProductOption> findByProductIdAndOptionName(Long productId, String optionName);
 }
